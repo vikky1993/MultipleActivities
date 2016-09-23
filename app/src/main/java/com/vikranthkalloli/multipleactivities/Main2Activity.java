fcc324bd36186327e93f57bbc2a58ea76467e18a
Vickranth <wickee0810@gmail.com>
2016-09-23 15:32:28 +0530

    Initial commit

---
+++ app/src/main/java/com/vikranthkalloli/multipleactivities/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.vikranthkalloli.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        Intent i = getIntent();
+        textView.setText("Hello " + i.getStringExtra("name"));
+      
+    }
+
+}
---
+++ app/src/main/java/com/vikranthkalloli/multipleactivities/MainActivity.java
@@ -0,0 +1,72 @@
+package com.vikranthkalloli.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> friends = new ArrayList<String>();
+
+        friends.add("vikky");
+        friends.add("keerth");
+        friends.add("aksh");
+        friends.add("me");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends);
+
+        listview.setAdapter(arrayAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+
+                i.putExtra("name", friends.get(position));
+
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
